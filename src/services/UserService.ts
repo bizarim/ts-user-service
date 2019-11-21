4519217b5730cf381e659f964a74fc79be0fd097
bizarim <bizarim82@gmail.com>
2019-11-21 16:46:51 +0900

    user service modify  Changes to be committed: 	modified:   src/services/UserService.ts

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -7,11 +7,18 @@ import { UserDto } from '../dto/UserDto';
 @Service()
 export class UserService {
     constructor(
+        @InjectRepository()
+        private readonly userRepository: UserRepository
     ) { }
+        public async test(): Promise<string> {
+            return 'test';
+        }
 
-    public async test(): Promise<string> {
+    public async create(userDto: UserDto): Promise<User> {
         console.log('UserService test');
-        return 'test';
+        const user = userDto.toUser();
+        this.userRepository.create(user);
+        return user;
     }
 
 }
